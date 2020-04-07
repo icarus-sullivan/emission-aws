5f89f4f4ec9219073df02d6ddcd1eabaf6c32d77
Chris <chrissullivan.dev@gmail.com>
2020-04-07 04:00:34 -0600

    Changing mapping for kinesis message

--- src/kinesis/index.js
+++ src/kinesis/index.js
@@ -2,17 +2,18 @@ const AWS = require('aws-sdk');
 
 const K = new AWS.Kinesis({ apiVersion: '2013-12-02' });
 
-// Auto format data for kinesis
-const formatData = (s) =>
-  Buffer.isBuffer(s) || typeof s === 'string' ? s : JSON.stringify(s);
+const batchRecordMap = ({ id, content }) => ({
+  Data:
+    Buffer.isBuffer(content) || typeof content === 'string'
+      ? content
+      : JSON.stringify(content),
+  PartitionKey: id,
+});
 
 // Proxy batch write to Kinesis
 const batch = ({ streamName, items }) =>
   K.putRecords({
-    Records: items.map(({ id, content }) => ({
-      Data: formatData(content),
-      PartitionKey: id,
-    })),
+    Records: items.map(batchRecordMap),
     StreamName: streamName,
   }).promise();
 
